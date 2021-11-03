838008e903d9b2ad5df5f04c09871caf6540de27
Akil Gray <successfulpride@gmail.com>
2021-11-03 16:53:47 -0400

    static routing path changed

--- server.js
+++ server.js
@@ -37,9 +37,9 @@ app.use(cors());
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
 
-app.use(express.static(path.join(__dirname, './client/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build'));
+app.use(express.static('./client/build'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 app.listen(port, () => {
